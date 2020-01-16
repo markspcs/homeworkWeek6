1534ec0f236f656fb57f995c5e2edde03c25d757
Stout <Mark.Stout@sprint.com>
2020-01-16 16:35:28 -0600

    completed left side column

--- script.js
+++ script.js
@@ -7,9 +7,57 @@ const imgDir = "./images/";
 // forecast https://api.openweathermap.org/data/2.5/forecast/daily?q=kansas+city&cnt=5&appid=166a433c57516f51dfab1f7edaed8413
 // UV https://samples.openweathermap.org/data/2.5/uvi?lat=37.75&lon=-122.37&appid=b6907d289e10d714a6e88b30761fae22
 var city = "kansas+city"
-getWeather(city);
-getForecast(city);
+var searchBox = $("#searchBox");
+var searchBtn = $("#searchBtn");
+//var storedCities = [];
+var storedItems = JSON.parse(localStorage.getItem("searchedCities"));
+var storedCities = storedItems ? storedItems : [];
+console.log(storedCities);
+writeStoredCities(storedCities);
 
+$(searchBtn).click(function () {
+  let searchCity = searchBox.val().toLowerCase();
+  getWeather(searchCity);
+  getForecast(searchCity);
+  orderCities(searchCity);
+  
+  console.log(searchCity);
+})
+$(".list-group-item").click(function () {
+  let clickedCity = $(this).attr("city-search");
+  console.log(clickedCity);
+  getWeather(clickedCity);
+  getForecast(clickedCity);
+})
+//////////////////////////////////////////
+//orders, and dedups and stores array of cities
+function orderCities(city){
+  console.log(city);
+  let indexed = storedCities.indexOf(city);
+  console.log(indexed);
+  if (indexed < 0) {
+    storedCities.unshift(city);
+
+    console.log("not stored");
+  }
+  else {
+    storedCities.splice(indexed, 1);
+    storedCities.unshift(city);   
+  }
+  localStorage.setItem("searchedCities", JSON.stringify(storedCities));
+  $(".list-group").empty();
+  writeStoredCities(storedCities);
+  //console.log();
+}
+////////////////////////////////////////////
+// writes left column of prevouis stored cities
+function writeStoredCities(storedCities){
+
+  storedCities.forEach(function(city){
+  //console.log(storedCities);
+  $(".list-group").append("<li class='list-group-item' city-search='" + city + "'" + ">" + city + "</li>");
+  });
+}
 /////////////////////////////////////////////
 //formats  5 day forecast to the bottom of the screen
 function forecastWeather(forecast) {
@@ -41,6 +89,7 @@ function getForecast(city) {
     .then(function (response) {
       //forecastWeather(response.list);
       response.list.shift();
+      $(".forecastBoxes").empty();
       response.list.forEach(forecastWeather);
     });
 }
@@ -63,13 +112,13 @@ function currentWeather(weatherObj) {
   $("#city").text(city);
   $("#date").text(month + "/" + day + "/" + year);
   $("#icon").attr("src", icon);
-  $("#temp").append(fahrTemp + " &#8457;");
-  $("#humidity").append(humidity + "%");
-  $("#wind").append(wind + " MPH");
+  $("#temp").html("Temperature: " + fahrTemp + " &#8457; ");
+  $("#humidity").html("Humidity: " + humidity + "%");
+  $("#wind").html("Wind Speed: " + wind + " MPH");
   //$("#uv").append("followUp");
   console.log(city + " " + lat + " " + long + " " + fahrTemp + " " + humidity + " " + month + "/" + day + "/" + year);
   let uvObj = getUv(lat, long);
-  uvObj.then(uvIndex => $("#uv").append(uvIndex.value));
+  uvObj.then(uvIndex => $("#uv").html("UV Index: " + uvIndex.value));
   //console.log(promis.then);
   //$("#uv").append(getUv(lat, long));
   return [lat, long];
